e2902c2f4cb7215493fadc6792bbd1dfd576b2b1
kh8333880-droid <kh8333880@gmail.com>
2026-07-31 23:41:29 +0300

    Add brand slider to homepage

--- app/data/products.ts
+++ app/data/products.ts
@@ -66,53 +66,251 @@ export const products = [
 
   category: "Soft Fruity Floral",
 },
+ {
+  id: "arrogate-pink-diva",
+  name: "Arrogate Pink Diva",
+  brand: "Assaf",
+  price: 2000,
+  image: "/arrogate-pink-diva.png",
+
+  topNotes: [
+    "Bergamot",
+    "Lychee",
+    "Pear",
+  ],
+
+  heartNotes: [
+    "Jasmine",
+    "Turkish Rose",
+    "Peony",
+  ],
+
+  baseNotes: [
+    "Musk",
+    "Vanilla",
+    "Sandalwood",
+    "Patchouli",
+  ],
+
+  sillage: "Medium to Strong",
+
+  longevity: "6-8 Hours",
+
+  occasion: "Autumn • Winter • Evening • Special Occasions",
+
+  category: "Luxury Floral Fruity Feminine",
+},
+ {
+  id: "laverne-sense",
+  name: "Laverne Sense",
+  brand: "Laverne",
+  price: 2000,
+  image: "/laverne-sense.png",
+
+  topNotes: [
+    "Bergamot",
+    "Orange",
+    "Green Apple",
+  ],
+
+  heartNotes: [
+    "Rose",
+    "Jasmine",
+    "Lily of the Valley",
+  ],
+
+  baseNotes: [
+    "Musk",
+    "Amber",
+    "Vanilla",
+    "Sandalwood",
+  ],
+
+  sillage: "Medium",
+
+  longevity: "5-7 Hours",
+
+  occasion: "Spring • Summer • Daily Wear • Work",
+
+  category: "Elegant Soft Floral",
+},
+ {
+  id: "box-extreme-laverne",
+  name: "Box Extreme Laverne",
+  brand: "Laverne",
+  price: 3500,
+  image: "/box-extreme-laverne.png",
+
+  topNotes: [
+    "Red Berries",
+    "Bergamot",
+    "Pear",
+  ],
+
+  heartNotes: [
+    "Turkish Rose",
+    "Jasmine",
+    "Peony",
+  ],
+
+  baseNotes: [
+    "White Musk",
+    "Vanilla",
+    "Amber",
+    "Sandalwood",
+  ],
+
+  sillage: "High",
+
+  longevity: "8-10 Hours",
+
+  occasion: "Winter • Autumn • Gifts • Evening",
+
+  category: "Luxury Floral Fruity Feminine",
+
+  includes: [
+    "Perfume",
+    "Body Lotion",
+    "Body Spray",
+  ],
+},
   {
-    id: "arrogate-pink-diva",
-    name: "Arrogate Pink Diva",
-    brand: "Assaf",
-    price: 2000,
-    image: "/arrogate-pink-diva.png",
-  },
-  {
-    id: "laverne-sense",
-    name: "Laverne Sense",
-    brand: "Laverne",
-    price: 2000,
-    image: "/laverne-sense.png",
-  },
-  {
-    id: "box-extreme-laverne",
-    name: "Box Extreme Laverne",
-    brand: "Laverne",
-    price: 3500,
-    image: "/box-extreme-laverne.png",
-  },
-  {
-    id: "box-moon-abrag",
-    name: "Box Moon Abrag",
-    brand: "Ibraq",
-    price: 3200,
-    image: "/box-moon-abrag.png",
-  },
-  {
-    id: "kahlilan",
-    name: "Kahlilan",
-    brand: "DKHOONE",
-    price: 1650,
-    image: "/kahlilan.png",
-  },
-  {
-    id: "signature-white",
-    name: "Signature White",
-    brand: "DKHOONE",
-    price: 1650,
-    image: "/signature-white.png",
-  },
-  {
-    id: "pearl-musk",
-    name: "Pearl Musk",
-    brand: "Reef",
-    price: 1650,
-    image: "/pearl-musk.png",
-  },
+  id: "box-moon-abrag",
+  name: "Box Moon Abrag",
+  brand: "Ibraq",
+  price: 3200,
+  image: "/box-moon-abrag.png",
+
+  topNotes: [
+    "Bergamot",
+    "Mint",
+    "Lavender",
+  ],
+
+  heartNotes: [
+    "Cinnamon",
+    "Cedarwood",
+    "Musk",
+  ],
+
+  baseNotes: [
+    "Amber",
+    "Patchouli",
+    "Oud Wood",
+    "Vanilla",
+  ],
+
+  sillage: "Very Strong",
+
+  longevity: "8-12 Hours",
+
+  occasion: "Winter • Evening • Formal Occasions",
+
+  category: "Luxury Oriental Woody Masculine",
+
+  includes: [
+    "3 Perfumes",
+  ],
+},
+ {
+  id: "kahlilan",
+  name: "Kahlilan",
+  brand: "Assaf",
+  price: 1650,
+  image: "/kahlilan.png",
+
+  topNotes: [
+    "Cardamom",
+    "Saffron",
+    "Bergamot",
+  ],
+
+  heartNotes: [
+    "Rose",
+    "Oud",
+    "Incense",
+  ],
+
+  baseNotes: [
+    "Amber",
+    "Musk",
+    "Oud Wood",
+    "Patchouli",
+  ],
+
+  sillage: "Very Strong",
+
+  longevity: "10-12 Hours",
+
+  occasion: "Winter • Evening • Special Occasions • Formal Events",
+
+  category: "Luxury Oriental Oud • Masculine / Unisex",
+},
+ {
+  id: "signature-white",
+  name: "Signature White",
+  brand: "Laverne",
+  price: 1650,
+  image: "/signature-white.png",
+
+  topNotes: [
+    "Green Apple",
+    "Bergamot",
+    "Lemon",
+  ],
+
+  heartNotes: [
+    "Lavender",
+    "Lily",
+    "Rose",
+  ],
+
+  baseNotes: [
+    "White Musk",
+    "Sandalwood",
+    "Soft Vanilla",
+  ],
+
+  sillage: "Medium",
+
+  longevity: "6-8 Hours",
+
+  occasion: "Spring • Summer • Daily Wear • Work • University",
+
+  category: "Fresh Aquatic Floral • Unisex",
+},
+ {
+  id: "pearl-musk",
+  name: "Pearl Musk",
+  brand: "Reef",
+  price: 1650,
+  image: "/pearl-musk.png",
+
+  topNotes: [
+    "White Musk",
+    "Bergamot",
+    "Pear",
+  ],
+
+  heartNotes: [
+    "Jasmine",
+    "Rose",
+    "Peony",
+  ],
+
+  baseNotes: [
+    "Powdery Musk",
+    "Vanilla",
+    "Sandalwood",
+    "White Amber",
+  ],
+
+  sillage: "Medium",
+
+  longevity: "6-8 Hours",
+
+  occasion: "Spring • Summer • Daily Wear • Work • Gifts",
+
+  category: "Soft Clean Musky Feminine",
+},
 ];
